06a918d9e7c9d62bb69ab288d9be7800b44f6050
Max Zykin <sanchez.max@yandex.ru>
2023-11-16 22:54:44 +0400

    Initial Commit

---
+++ Milestone19-21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone19-21
+//
+//  Created by Максим Зыкин on 16.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
